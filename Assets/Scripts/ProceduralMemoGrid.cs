9e63d5e73c0c68f49b2a68c5e169b57794606789
Adriano Gil <adrianogil.san@gmail.com>
2018-01-02 21:15:06 -0400

    Procedural grid generation

---
+++ Assets/Scripts/ProceduralMemoGrid.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ProceduralMemoGrid : MonoBehaviour {
+
+    public Material itemMaterial;
+    public float cameraDistance = 5f;
+    public float paddingXPercent = 0.1f;
+    public float paddingYPercent = 0.1f;
+    public int gridSizeX;
+    public int gridSizeY;
+
+	// Use this for initialization
+	void Start () {
+		Generate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Generate()
+    {
+        Vector3 gridPoint = Camera.main.transform.position + 
+            cameraDistance * Camera.main.transform.forward;
+
+        float height = 2f * cameraDistance * Mathf.Tan(0.5f * Camera.main.fieldOfView * Mathf.Deg2Rad);
+        float width = height * (Screen.width * 1f/ Screen.height);
+
+        Vector3 left = (-1f)*Camera.main.transform.right;
+        Vector3 up = Camera.main.transform.up;
+
+        Vector3 upperLeftPoint = gridPoint + 
+            0.5f * width * left + 
+            0.5f * height * up;
+
+        Vector2 gridItemSize = new Vector2(width * (1f/gridSizeX),height * (1f/gridSizeY));
+
+        upperLeftPoint.x += gridItemSize.x;
+        // upperLeftPoint.y -= 0.25f * gridItemSize.y;
+
+        Vector2 gridItemInternalSize = Vector2.Scale(new Vector2(1f - paddingXPercent, 1f - paddingYPercent),
+            gridItemSize);
+
+        GameObject go;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 gridItemPos = upperLeftPoint - x * gridItemSize.x * left - y * gridItemSize.y * up;
+                go = ProceduralMemoItem.Generate(gridItemInternalSize,left,(-1f)*up, itemMaterial);
+                
+                go.transform.position = gridItemPos;
+            }
+        }
+
+        Debug.Log("GilLog - ProceduralMemoGrid::Generate - height " + height + "  - width " + width + " ");        
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ProceduralMemoGrid))]
+public class ProceduralMemoGridEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    
+        ProceduralMemoGrid editorObj = target as ProceduralMemoGrid;
+    
+        if (editorObj == null) return;
+
+        if (GUILayout.Button("Generate"))
+        {
+            editorObj.Generate();
+        }
+    }
+}
+#endif
+
+
--- Assets/Scripts/ProceduralMemoItem.cs
+++ Assets/Scripts/ProceduralMemoItem.cs
@@ -2,7 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class ProceduralMemoItem : MonoBehaviour {
+
+	public Material material;
+    public Vector3 downDirection;
+    public Vector3 leftDirection;
+    public Vector2 gridItemSize;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +22,59 @@ public class ProceduralMemoItem : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void Generate()
+    {
+        MeshBuilder meshBuilder = new MeshBuilder();
+        meshBuilder.AddQuad(Vector3.zero, 
+            gridItemSize.x * leftDirection, 
+            gridItemSize.y * downDirection);
+
+		Mesh mesh = meshBuilder.CreateMesh ();
+
+		MeshFilter filter = gameObject.GetComponent<MeshFilter> ();
+		if (filter == null)
+			filter = gameObject.AddComponent<MeshFilter> ();
+		filter.mesh = mesh;
+
+		MeshRenderer renderer = gameObject.GetComponent<MeshRenderer> ();
+		if (renderer == null)
+			renderer = gameObject.AddComponent<MeshRenderer> ();
+		renderer.material = material;
+	}
+
+    public static GameObject Generate(Vector2 gridItemSize, Vector3 left, Vector3 down, Material material)
+    {
+        GameObject go = new GameObject("MemoItem");
+        ProceduralMemoItem memoItem = go.AddComponent<ProceduralMemoItem>();
+
+        memoItem.material = material;
+        memoItem.gridItemSize = gridItemSize;
+        memoItem.downDirection = down;
+        memoItem.leftDirection = left;
+
+        memoItem.Generate();
+
+        return go;
+    }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ProceduralMemoItem))]
+public class ProceduralMemoItemEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    
+        ProceduralMemoItem editorObj = target as ProceduralMemoItem;
+    
+        if (editorObj == null) return;
+
+        if (GUILayout.Button("Generate")) {
+            editorObj.Generate();
+        }
+    }
+
+}
+#endif
