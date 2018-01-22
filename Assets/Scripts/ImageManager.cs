32add87d84f826161ba33039ef12cc38186f67b2
Adriano Gil <adrianogil.san@gmail.com>
2018-01-21 22:23:58 -0400

    Added sample images

---
+++ Assets/Scripts/ImageManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class ImageData
+{
+    public string name;
+    public Texture2D texture;
+}
+
+
+public class ImageManager : MonoBehaviour {
+
+    public List<ImageData> images;
+
+	// Use this for initialization
+	void Start () {
+	
+        SaveImagesInto();
+
+	}
+	
+	void SaveImagesInto () {
+
+        Debug.Log("GilLog - ImageManager::SaveImagesInto - "  + Application.persistentDataPath);
+
+        byte[] imageBytes;
+        string imagePath = "";
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            // Save Texture2D into file
+            imageBytes = images[i].texture.EncodeToPNG();
+
+            imagePath = Application.persistentDataPath + "/images/" + images[i].name + ".png";
+            File.WriteAllBytes(imagePath, imageBytes);
+            Debug.Log("GilLog - ImageManager::SaveImagesInto - Saved image " + imagePath);
+        }
+	}
+}
